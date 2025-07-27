f1c34532cb0f4a45f1a07a1e5d350f8a5cb9fc01
Jayant Garg <gargjayant1983@gmail.com>
2025-07-28 00:56:53 +0530

    faculty testimonials

--- src/Pages/Home.page.jsx
+++ src/Pages/Home.page.jsx
@@ -2,12 +2,14 @@ import HeroBanner from "@/components/Sections/Home/Hero.banner";
 import FAQ from "@/components/Sections/Home/FAQ";
 import PastEvents from "@/components/Sections/Home/PastEvents";
 import { Fragment } from "react";
+import FacultyTestimonials from "@/components/Sections/Home/FacultyTestimonials";
 
 export default function HomePage() {
   return (
     <Fragment>
       <HeroBanner />
       <PastEvents />
+      <FacultyTestimonials />
       <FAQ />
     </Fragment>
   );
