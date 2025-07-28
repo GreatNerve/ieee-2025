297ed7970cf2faf4505825b0e924b4b9f8b29155
Janya Goel <janyagoel.14@gmail.com>
2025-07-28 21:12:08 +0530

    Updated Home.page.jsx

--- src/Pages/Home.page.jsx
+++ src/Pages/Home.page.jsx
@@ -1,6 +1,7 @@
 import HeroBanner from "@/components/Sections/Home/Hero.banner";
 import FAQ from "@/components/Sections/Home/FAQ";
 import PastEvents from "@/components/Sections/Home/PastEvents";
+import AlumniSection from "@/components/Section/Home/AlumniSection";
 import { Fragment } from "react";
 
 export default function HomePage() {
